f3593b10031dd85ee700c717ffeb26e23b9e4bf2
Sankar Biswas <sankarb475@gmail.com>
2019-01-24 16:57:29 +0530

    Update pythonBasicsHandsOn.py

--- pythonBasicsHandsOn.py
+++ pythonBasicsHandsOn.py
@@ -3,11 +3,17 @@ The interactive prompt runs code and echoes results as you go, but it doesn’t 
 '''
 
 #to get current working directory
-
 >>> import os
 >>> os.getcwd()
 '/Users/sankar.biswas'
 
+#changing current direcctory 
 >>> os.chdir('/Users/sankar.biswas/Desktop/Python/coding')
 >>> os.getcwd()
 '/Users/sankar.biswas/Desktop/Python/coding'
+
+# to run a python script from command prompt
+python file1.py
+
+#saving the output in a file 
+python script1.py > saveit.txt
