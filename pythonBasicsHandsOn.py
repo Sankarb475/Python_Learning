84aba24caaf316b95b870749baeae90d9579d677
Sankar Biswas <sankarb475@gmail.com>
2019-01-24 16:30:33 +0530

    Update pythonBasicsHandsOn.py

--- pythonBasicsHandsOn.py
+++ pythonBasicsHandsOn.py
@@ -1,1 +1,13 @@
-# to run python in command prompt, use "python", (windows :considering you have set up environment variable)
+'''to run python in command prompt, use "python", (windows :considering you have set up environment variable)
+The interactive prompt runs code and echoes results as you go, but it doesn’t save your code in a file
+'''
+
+#to get current working directory
+
+>>> import os
+>>> os.getcwd()
+'/Users/sankar.biswas'
+
+>>> os.chdir('/Users/sankar.biswas/Desktop/Python/coding')
+>>> os.getcwd()
+'/Users/sankar.biswas/Desktop/Python/coding'
