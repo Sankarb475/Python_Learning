b9ce1ae384585a8887dfdcfdaf49a1ee1a32f8fc
Sankar Biswas <sankarb475@gmail.com>
2019-01-24 14:55:23 +0530

    Update pythonBasics.py

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,1 +1,17 @@
-'in-progress'
+''' Why python?
+
+==> object-oriented scripting language which has functional programming capability.
+
+software quality ==> readble, coherence, software quality, reusable, maintainability 
+
+Developer productivity ==> lines of code is much less in volume with respect to C++ or Java that is less to type, debug and 
+easy maintainability
+
+Program portability ==> with respect to version or OS (linux and windows) doesnt trouble us much when importing the code from
+one OS to another
+
+Support libraries ==> self-explanatory 
+
+Downside ==> a bit slower than other low level language like C or C++
+
+'''
