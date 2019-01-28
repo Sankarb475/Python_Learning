27022afc790749933493f975c603d09a00fa03ba
Sankar Biswas <sankarb475@gmail.com>
2019-01-28 13:53:00 +0530

    Create pythonCoreDataTypes.py

---
+++ pythonCoreDataTypes.py
@@ -0,0 +1,23 @@
+>>> from math import sqrt
+>>> sqrt(16)
+4.0
+>>> import math
+>>> math.pi
+3.141592653589793
+
+>>> dir(math)
+['__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2',
+'atanh', 'ceil', 'copysign', 'cos', 'cosh', 'degrees', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod'
+, 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'ldexp', 'lgamma', 'log', 'log10', 
+'log1p', 'log2', 'modf', 'nan', 'pi', 'pow', 'radians', 'remainder', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'tau', 'trunc']
+
+# The math module contains more advanced numeric tools as functions, while the ran dom module performs random-number generation 
+# and random selections
+
+>>> import random
+>>> random.random() 
+0.7082048489415967
+>>> random.choice([1, 2, 3, 4]) 
+1
+
+
