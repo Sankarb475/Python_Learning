5f1736a6e705d6831aa42fca0e8d3f1b4131f0ac
Sankar Biswas <sankarb475@gmail.com>
2019-01-28 15:21:48 +0530

    Create dataTypeConversion.py

---
+++ dataTypeConversion.py
@@ -0,0 +1,24 @@
+#List to String
+>>> a = ['l','i','f','e']
+>>> ''.join(a)
+'life'
+
+>>> ','.join(a)
+'l,i,f,e'
+
+#String to List
+>>> a = 'Beautiful'
+>>> b = list(a)
+>>> b
+['B', 'e', 'a', 'u', 't', 'i', 'f', 'u', 'l']
+
+#List to Set
+>>> a = [1,2,1]
+>>> b = set(a)
+>>> b
+{1, 2}
+
+#Set to List
+>>> c = list(b)
+>>> c
+[1, 2]
