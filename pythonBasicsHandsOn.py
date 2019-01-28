86ecf10c555eaa7c4d5e178c554d493d9bf4d91f
Sankar Biswas <sankarb475@gmail.com>
2019-01-28 15:47:52 +0530

    Update pythonBasicsHandsOn.py

--- pythonBasicsHandsOn.py
+++ pythonBasicsHandsOn.py
@@ -33,3 +33,22 @@ python script1.py > saveit.txt
  'setswitchinterval', 'settrace', 'stderr', 'stdin', 'stdout', 'thread_info', 'version', 'version_info', 'warnoptions']
 
  # the " exec(open('module.py').read())" built-in function call is another way to launch files from the interactive prompt without having to import and later reload
+
+ #you can also find out the functions you can apply on a variable using "dir"
+ >>> a = 234
+>>> dir(a)
+['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', 
+ '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__gt__', 
+ '__hash__', '__index__', '__init__', '__init_subclass__', '__int__', '__invert__', '__le__', '__lshift__', '__lt__', '__mod__'
+ , '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', 
+ '__reduce_ex__', '__repr__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', 
+ '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', 
+ '__trunc__', '__xor__', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 'numerator', 'real', 'to_bytes']
+
+
+
+
+
+
+
+
