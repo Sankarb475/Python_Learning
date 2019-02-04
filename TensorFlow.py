b64d965959dde25ae4e33df212a8fa02c868fe7f
Sankar Biswas <sankarb475@gmail.com>
2019-02-04 20:19:37 +0530

    Update TensorFlow.py

--- TensorFlow.py
+++ TensorFlow.py
@@ -1,3 +1,14 @@
-#installing Tensorflow using pip
+#upgrading pip
+python3 -m pip install --upgrade pip
 
+#installing jupyter notebook
+python3 -m pip install jupyter
+
+#installing virtualenv
+pip install virtualenv
+
+#installing Tensorflow using pip
 python3 -m pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.12.0-py3-none-any.whl
+  
+# run "jupyter notebook" from command prompt to run jupyter notebook
+
