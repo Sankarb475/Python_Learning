f3459ff1733e70102ec561ad191603a8603f3ea5
Sankar Biswas <sankarb475@gmail.com>
2019-02-06 17:54:12 +0530

    Update FileOperation.py

--- FileOperation.py
+++ FileOperation.py
@@ -1,3 +1,21 @@
+'''
+Python provides two built-in functions to read a line of text from standard input, which by default comes from the keyboard. 
+These functions are −
+
+1) raw_input
+2) input
+'''
+
+#The raw_input([prompt]) function reads one line from standard input and returns it as a string (removing the trailing newline).
+# raw_input doesnt work after Python 3.0
+
+>>> str = raw_input("Enter your input: ");
+>>> print "Received input is : ", str
+
+#input([prompt]) function is equivalent to raw_input, except that it assumes the input is a valid Python expression and 
+#returns the evaluated result to you before python 3.0
+#after python 3.0 raw_input has been renamed to input()
+
 #writing something to a file
 
 >>> dataFile = open('/Users/sankar.biswas/Desktop/data.txt', 'w')
@@ -12,6 +30,8 @@
 I am in Love
 >>> data.split(' ')
 ['I', 'am', 'in', 'Love']
+
+
 
 
 
