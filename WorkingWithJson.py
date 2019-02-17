d421899cd881d277b90b48849705d338ff7ba605
Sankar Biswas <sankarb475@gmail.com>
2019-02-17 21:32:43 +0530

    Update WorkingWithJson.py

--- WorkingWithJson.py
+++ WorkingWithJson.py
@@ -1,8 +1,17 @@
+# working and playing with JSON file 
 
 >>> import json
-
 >>> json1_file = open('jsonFile.json')
-
 >>> json1_str = json1_file.read()
 
->>> json1_str
+>>> type(json1_str)
+<class 'str'>
+
+>>> json1_data = json.loads(json1_str)
+>>> type(json1_data)
+<class 'dict'>
+
+ # json1_data is a dictionary, and if your dictionary keys are storing dictionary value, it will be a dictioanry of dictionary
+ # based on my input data, my json1_data is a dictionary of dictionary of a list.
+
+
