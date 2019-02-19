470614617fc404d9a6c8e221e97f7f36ebcd2713
Sankar Biswas <sankarb475@gmail.com>
2019-02-19 15:52:15 +0530

    Update SmallHelpfulCodeSnipet.py

--- SmallHelpfulCodeSnipet.py
+++ SmallHelpfulCodeSnipet.py
@@ -16,3 +16,17 @@
 >>> sorted_x = sorted(x.items(), key=operator.itemgetter(1))
 >>> sorted_x
 [(0, 0), (2, 1), (1, 2), (4, 3), (3, 4)]
+
+
+# switching keys and values in a dictionary in python
+>>> my_dict = {'a':1,'b':2}
+>>> my_dict2 = {y:x for x,y in my_dict.items()}
+>>> my_dict2
+{1: 'a', 2: 'b'}
+
+
+# Convert a String representation of a Dictionary to a dictionary
+>>> import ast
+>>> ast.literal_eval("{'muffin' : 'lolz', 'foo' : 'kitty'}")
+{'muffin': 'lolz', 'foo': 'kitty'}
+
