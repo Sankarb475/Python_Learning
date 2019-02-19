a1c599afed8d1027ff76061e8d42fc29a0776be9
Sankar Biswas <sankarb475@gmail.com>
2019-02-19 15:47:04 +0530

    Update SmallHelpfulCodeSnipet.py

--- SmallHelpfulCodeSnipet.py
+++ SmallHelpfulCodeSnipet.py
@@ -8,3 +8,11 @@
 >>> b.append([1]*6)
 >>> b
 [[1, 1, 1, 1, 1, 1]]
+
+
+# sorting a dictionary based on the values ::
+>>> import operator
+>>> x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+>>> sorted_x = sorted(x.items(), key=operator.itemgetter(1))
+>>> sorted_x
+[(0, 0), (2, 1), (1, 2), (4, 3), (3, 4)]
