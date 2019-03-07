36f2a8f2eeaff651969435d055d40e0c5311b6d6
Sankar Biswas <sankarb475@gmail.com>
2019-03-07 14:30:50 +0530

    Update QuestionsAnswers.py

--- QuestionsAnswers.py
+++ QuestionsAnswers.py
@@ -1,6 +1,6 @@
 1) Adding two dictionary in python :: use update method
 
-if you are using Python 2.x :: 
+==> if you are using Python 2.x :: 
 
 >>> def merge_two_dicts(x, y):
 ...     z = x.copy()
@@ -31,3 +31,33 @@ in python 3.5 or greater ::
 
 
 ==============================================================================================================================
+2) how to run a unix/linux command through a python script?
+
+==> with the help of this package :: import subprocess
+
+file_name = "file_to_be_copied.txt"
+file_path = "file/path"
+fileDet = file_path + "/" + file_name
+folderPath = "/ngs/app/odsp/AutomationScript/FinalCode/data_archival/getFiles"
+shellQuery = """
+             cp '{}' '{}'
+             """.format(fileDet, folderPath)
+subprocess.Popen([shellQuery], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+
+
+if your shell script returns some output and you want to catch it and use it later ::
+
+process = subprocess.Popen([shellQuery], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+output = process.stdout.read()
+output1 = output.split("\n")
+lines = [line for line in output1 if line.strip()]
+out = lines[2:-1]
+
+
+
+
+
+
+
