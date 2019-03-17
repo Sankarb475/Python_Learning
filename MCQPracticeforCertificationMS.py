58f3c75eb3516ff880c0efb48c841c2baa59c713
Sankar Biswas <sankarb475@gmail.com>
2019-03-17 10:08:12 +0530

    Update MCQPracticeforCertificationMS.py

--- MCQPracticeforCertificationMS.py
+++ MCQPracticeforCertificationMS.py
@@ -12,7 +12,7 @@ outside F 100
 Inside F 112
 
 ==============================================================================================================================
-
+global f
 f = 100
 print("outside F", f)
 def meth1(a):
@@ -22,9 +22,9 @@ def meth1(a):
     return f
 
 def meth2(a):
-    global f
     f = a + 10
     f = f + meth1(a)
+    print(dir())
     print(f)
 
 meth2(10)
@@ -32,10 +32,39 @@ meth2(10)
 output ==>
 
 outside F 100
-Inside F 130
-250
+Inside F 110
+['a', 'f']
+130
+
+dir() ==> Returns a list of names in current local scope or a list of object attributes
 
 ==============================================================================================================================
+deleting a variable already defined
+>>> del a
+
+==============================================================================================================================
+Printing Raw String ::
+>>> print('foo\nbar')
+foo
+bar
+>>> print(r'foo\nbar')
+foo\nbar
+>>> print(R'foo\nbar')
+foo\nbar
+
+==============================================================================================================================
+>>> a = 23
+>>> b = 24
+>>> eval("a+b")
+47
+
+
+
+
+
+
+
+
 
 
 
