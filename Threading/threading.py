2a193a34b2e5659c44f05b0b81381867837a373b
Sankar Biswas <sankarb475@gmail.com>
2020-01-26 19:59:55 +0530

    Create threading.py

---
+++ Threading/threading.py
@@ -0,0 +1,5 @@
+Running several threads is similar to running several different programs concurrently, but with the following benefits −
+
+1) Multiple threads within a process share the same data space with the main thread and can therefore share information or communicate 
+with each other more easily than if they were separate processes.
+2)Threads sometimes called light-weight processes and they do not require much memory overhead; they are cheaper than processes.
