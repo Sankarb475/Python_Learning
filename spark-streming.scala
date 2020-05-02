b60d49e04a8374291c0fb9dfc4eb240f4e347e76
Sankar Biswas <sankarb475@gmail.com>
2020-05-02 11:34:56 +0530

    Update spark-streming.scala

--- spark-streming.scala
+++ spark-streming.scala
@@ -1,49 +1,37 @@
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.streaming.kafka010._
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+import scala.io.Source
 
 
-object KafkaSpark extends App{
-  System.setProperty("hadoop.home.dir", "D:\\App\\hadoop_2.7.1")
+object KafkaProd extends App {
 
-  val conf = new SparkConf().setMaster("local[*]").setAppName("KafkaSparkStreaming")
-  val spark = SparkSession.builder().config(conf).getOrCreate()
+  val props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+  props.put("client.id", "ScalaProducerExample")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-  val sc = spark.sparkContext
+  val producer = new KafkaProducer[String, String](props)
 
-  val ssc = new StreamingContext(sc, Seconds(10))
+  val TOPIC="test-log"
 
-  val topicsSet = Set("test-log")
-
-
-  val kafkaParams = Map[String, Object](
-    "bootstrap.servers" -> "localhost:9092",
-    "key.deserializer" -> classOf[StringDeserializer],
-    "value.deserializer" -> classOf[StringDeserializer],
-    "group.id" -> "kafka_spark",
-    "auto.offset.reset" -> "latest"
+  val file = Source.fromFile("D:\\App\\data\\employee.csv")
+  val src = file.getLines()
+  // print the uid for Guest
+  src.foreach(
+    a => {
+      val record = new ProducerRecord(TOPIC, "key", a)
+      producer.send(record)
+    }
   )
+  file.close()
 
-  //consume the messages from Kafka topic and create DStream
-  val LogsStream = KafkaUtils
-    .createDirectStream[String, String](ssc, LocationStrategies.PreferConsistent, ConsumerStrategies.Subscribe[String, String]
-      (topicsSet,kafkaParams))
+  // val record = new ProducerRecord(TOPIC, "key", "the end "+new java.util.Date)
+  // producer.send(record)
 
-
-  val messages=LogsStream.map(_.value())
-  case class schema(ID: Int, First: String, Last: String, Salary: Int, Currency: String)
-
-  import spark.implicits._
-  val df = messages.map(x => x.split(',')).map(field => schema(field(0).toInt, field(1), field(2), field(3).toInt, field(4)))
-    .toDF("ID", "First", "Last", "Salary", "Currency")
-
-  val words=messages.flatMap(x=>x.split(','))
-  val counts=words.map(x=>(x,1)).reduceByKey(_+_)
-  counts.print()
+  producer.close()
 
 
-  ssc.start()
-  ssc.awaitTermination()
 }
